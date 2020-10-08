3738959c46157f1430abc0e11c95815d334d8900
Anderson Lira <anderson.lira@celfocus.com>
2020-10-08 17:20:57 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,35 +13,15 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Cotações',
+      url: '/folder/Analytics',
+      icon: 'analytics'
     },
     {
-      title: 'Outbox',
+      title: 'Configurações',
       url: '/folder/Outbox',
-      icon: 'paper-plane'
+      icon: 'construct'
     },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
